657ea73517848d8786ccd366b773e202246671e4
zenos <zenoslin@outlook.com>
2019-06-15 22:54:13 +0800

    feat: 验证路径不能为中文&使用Promise代替回调&用localStorage缓存

--- renderer.js
+++ renderer.js
@@ -1,92 +1,110 @@
-const {
-    ipcRenderer
-} = require('electron')
-const {
-    editJson,
-    atlas,
-    json2pd
-} = require('./src/atlas')
+const { ipcRenderer } = require("electron");
+const { editJson, atlas, json2pd } = require("./src/atlas");
+const os = require("os");
 
-const inputBtn = document.getElementById('input-btn')
-const outputBtn = document.getElementById('output-btn')
-const inputPath = document.getElementById('input-path')
-const outputPath = document.getElementById('output-path')
-const tinyBtn = document.getElementById('tiny-btn')
-const logSpan = document.getElementById('log-span')
-const devBtn = document.getElementById('dev-btn')
+const inputBtn = document.getElementById("input-btn");
+const outputBtn = document.getElementById("output-btn");
+const inputPath = document.getElementById("input-path");
+const outputPath = document.getElementById("output-path");
+const tinyBtn = document.getElementById("tiny-btn");
+const logSpan = document.getElementById("log-span");
+const devBtn = document.getElementById("dev-btn");
 
-let _inputPath = "/Users/linze/Documents/GitHub/atlas-electron/assets/comp"
-let _outputPath = "/Users/linze/Documents/GitHub/atlas-electron/bin"
-let jsonPath = __dirname + "/lib/atlasConfig"
+let desktopPath = `${os.homedir()}/Desktop`;
+let _inputPath = window.localStorage.inputPath
+  ? window.localStorage.inputPath
+  : desktopPath;
+let _outputPath = window.localStorage.outputPath
+  ? window.localStorage.outputPath
+  : desktopPath;
+let jsonPath = `${__dirname}/lib/atlasConfig`;
 
-inputPath.value = _inputPath
-outputPath.value = _outputPath
+inputPath.value = _inputPath;
+outputPath.value = _outputPath;
 
-inputBtn.addEventListener('click', (event) => {
-    console.log('点击输入按钮')
-    ipcRenderer.send('open-file-dialog-input')
-})
+inputBtn.addEventListener("click", event => {
+  ipcRenderer.send("open-file-dialog-input");
+});
 
-outputBtn.addEventListener('click', (event) => {
-    console.log('点击输出按钮')
-    ipcRenderer.send('open-file-dialog-output')
-})
+outputBtn.addEventListener("click", event => {
+  ipcRenderer.send("open-file-dialog-output");
+});
 
-devBtn.addEventListener('click', (event) => {
-    console.log('打开开发者工具')
-    ipcRenderer.send('open-dev-tool')
-})
+devBtn.addEventListener("click", event => {
+  ipcRenderer.send("open-dev-tool");
+});
 
-ipcRenderer.on('input-path', (event, path) => {
-    console.log(`收到完成信息 ${path}`)
-    _inputPath = path
-    inputPath.value = `${path}`
-})
+ipcRenderer.on("input-path", (event, path) => {
+  console.log(`收到完成信息 ${path}`);
+  _inputPath = path;
+  inputPath.value = `${path}`;
+});
 
-ipcRenderer.on('output-path', (event, path) => {
-    console.log(`收到完成信息 ${path}`)
-    _outputPath = path
-    outputPath.value = `${path}`
-})
+ipcRenderer.on("output-path", (event, path) => {
+  console.log(`收到完成信息 ${path}`);
+  _outputPath = path;
+  outputPath.value = `${path}`;
+});
 
-tinyBtn.addEventListener('click', (event) => {
-    _inputPath = inputPath.value
-    _outputPath = outputPath.value
-    logSpan.innerHTML = "正在打包"
-    editJson(jsonPath, "inputDir", _inputPath, () =>{
-        editJson(jsonPath, "outputDir", _outputPath, () => {
-            editJson(jsonPath, "resDir", _outputPath + "/img", () =>{
-                atlas(false, function () {
-                    let fileName = _inputPath.substr(_inputPath.lastIndexOf("/") + 1)
-                    json2pd(_outputPath + "/" + fileName + ".atlas", () => {
-                        logSpan.innerHTML = "打包完成"
-                        alert('打包完成')
-                    })
-                })
-            })
-        })
-    })    
-})
+tinyBtn.addEventListener("click", event => {
+  _inputPath = inputPath.value;
+  _outputPath = outputPath.value;
+  if (hasChinese(_inputPath) || hasChinese(_outputPath)) {
+    alert("不可有中文路径");
+    return;
+  }
+  if (window.localStorage) {
+    window.localStorage.inputPath = _inputPath;
+    window.localStorage.outputPath = _outputPath;
+  }
+  logSpan.innerHTML = "正在打包";
+  Promise.all([
+    editJson(jsonPath, "inputDir", _inputPath),
+    editJson(jsonPath, "outputDir", _outputPath),
+    editJson(jsonPath, "resDir", _outputPath + "/img")
+  ]).then(() => {
+    atlas(false)
+      .then(res => {
+        console.log(res);
+        let fileName = _inputPath.substr(_inputPath.lastIndexOf("/") + 1);
+        json2pd(_outputPath + "/" + fileName + ".atlas")
+          .then(() => {
+            logSpan.innerHTML = "打包完成";
+            alert("打包完成");
+          })
+          .catch(err => {
+            alert(err);
+          });
+      })
+      .catch(err => {
+        alert(err);
+      });
+  });
+});
 
 //Js拖入文件
-const holder = document.getElementById("holder")
+const holder = document.getElementById("holder");
 
-holder.ondragenter = holder.ondragover = (event) => {
-    event.preventDefault()
-    holder.className = "jumbotron holder-ondrag"
-}
+holder.ondragenter = holder.ondragover = event => {
+  event.preventDefault();
+  holder.className = "jumbotron holder-ondrag";
+};
 
-holder.ondragleave = (event) => {
-    event.preventDefault()
-    holder.className = "jumbotron holder"
-}
+holder.ondragleave = event => {
+  event.preventDefault();
+  holder.className = "jumbotron holder";
+};
 
-holder.ondrop = (event) => {
-    // 调用 preventDefault() 来避免浏览器对数据的默认处理
-    //（drop 事件的默认行为是以链接形式打开） 
-    event.preventDefault()
-    holder.className = "jumbotron holder"
-    var file = event.dataTransfer.files[0]
-    _inputPath = inputPath.value = file.path
-    console.log(_inputPath)
+holder.ondrop = event => {
+  // 调用 preventDefault() 来避免浏览器对数据的默认处理
+  //（drop 事件的默认行为是以链接形式打开）
+  event.preventDefault();
+  holder.className = "jumbotron holder";
+  let file = event.dataTransfer.files[0];
+  _inputPath = inputPath.value = file.path;
+};
+
+function hasChinese(message) {
+  let reg = new RegExp("[\\u4E00-\\u9FFF]+", "g");
+  return reg.test(message);
 }
--- src/atlas.js
+++ src/atlas.js
@@ -1,77 +1,84 @@
-const childProcess = require('child_process')
-const path = require('path')
-const fs = require('fs')
-const Byte = require('./Byte')
+const childProcess = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const Byte = require("./Byte");
 
-function atlas(cmd, callback) {
+function atlas(cmd) {
+  return new Promise((resolve, reject) => {
     if (!cmd) {
-        cmd = path.resolve(__dirname, '..') + "/lib/atlasGenerator" + " --config " + path.resolve(__dirname, '..') + "/lib/atlasConfig"
+      cmd =
+        path.resolve(__dirname, "..") +
+        "/lib/atlasGenerator" +
+        " --config " +
+        path.resolve(__dirname, "..") +
+        "/lib/atlasConfig";
     }
-    childProcess.exec(cmd, {
+    childProcess.exec(
+      cmd,
+      {
         encoding: "binary",
         maxBuffer: 1024 * 1024 * 20
-    }, (err, stdOut, stdErr) => {
-        console.log(err + stdErr)
-        console.log(stdOut)
-        callback()
-    })
+      },
+      (err, stdOut, stdErr) => {
+        if (err) reject(err, stdErr);
+        resolve(stdOut);
+      }
+    );
+  });
 }
 
-function editJson(path, key, value, callback) {
-    let data = fs.readFileSync(path, {encoding: "utf8"})
-    let configData = data.toString() //将二进制的数据转换为字符串
-    configData = JSON.parse(configData) //将字符串转换为json对象
+function editJson(path, key, value) {
+  return new Promise((resolve, reject) => {
+    let data = fs.readFileSync(path, { encoding: "utf8" });
+    let configData = data.toString(); //将二进制的数据转换为字符串
+    configData = JSON.parse(configData); //将字符串转换为json对象
     if (typeof value == "object") {
-        configData[key] = value[0]
+      configData[key] = value[0];
     } else {
-        configData[key] = value
+      configData[key] = value;
     }
-    let str = JSON.stringify(configData) //因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
-    fs.writeFileSync(path, str, {encoding: "binary"})
-    console.log('修改成功' + str)
-    if (callback) {
-        callback()
-    }
+    let str = JSON.stringify(configData); //因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
+    fs.writeFileSync(path, str, { encoding: "binary" });
+    resolve(`修改成功${str}`);
+  });
 }
 
-function json2pd(path, callback) {
-    let data = fs.readFileSync(path, {encoding: "utf8"})
-    let configData = data.toString() //将二进制的数据转换为字符串
-    configData = JSON.parse(configData) //将字符串转换为json对象
-    let count = 0
+function json2pd(path) {
+  return new Promise((resolve, reject) => {
+    let data = fs.readFileSync(path, { encoding: "utf8" });
+    let configData = data.toString(); //将二进制的数据转换为字符串
+    configData = JSON.parse(configData); //将字符串转换为json对象
+    let count = 0;
     for (let s in configData.frames) {
-        count++
+      count++;
     }
-    let totalData = new Byte()
-    totalData.writeInt32(count)
-    console.log(path + count)
+    let totalData = new Byte();
+    totalData.writeInt32(count);
     for (var k in configData.frames) {
-        var p = configData.frames[k]
-        totalData.writeUTFString(k)
-        totalData.writeInt16(p.frame["x"])
-        totalData.writeInt16(p.frame["y"])
-        totalData.writeInt16(p.frame["w"])
-        totalData.writeInt16(p.frame["h"])
+      var p = configData.frames[k];
+      totalData.writeUTFString(k);
+      totalData.writeInt16(p.frame["x"]);
+      totalData.writeInt16(p.frame["y"]);
+      totalData.writeInt16(p.frame["w"]);
+      totalData.writeInt16(p.frame["h"]);
     }
-    let arr = new Uint8Array(totalData.buffer)
-    console.log(arr)
-    let pdPath = path.replace(".atlas", ".pd")
-    fs.writeFileSync(pdPath, arr, {encoding: "binary"})
-    fs.unlink(path, (err) => {
-        if (err) {
-            // console.error(err)
-            alert(err)
-        }
-        console.log('删除' + path)
-        callback()
-    })
+    let arr = new Uint8Array(totalData.buffer);
+    let pdPath = path.replace(".atlas", ".pd");
+    fs.writeFileSync(pdPath, arr, { encoding: "binary" });
+    fs.unlink(path, err => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
 }
 
 module.exports = {
-    atlas,
-    editJson,
-    json2pd
-}
+  atlas,
+  editJson,
+  json2pd
+};
 
 // //解析需要遍历的文件夹，我这以E盘根目录为例
 // var filePath = path.resolve('/Users/linze/Documents/GitHub/atlas-electron/bin')
